80beca776295917dc415206271bdf0561215455d
Javier Gonzalez <javgon2306@gmail.com>
2024-10-04 01:29:08 -0300

    act app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from "react"
 
+const CAT_ENDPOINT_RAMDOM_FACT = 'https://catfact.ninja/fact';
+// const CAT_ENDPOINT_IMG_URL = `https://cataas.com/cat/says/${firstWord}?`;
+
 function App() {
-  const [fact, setFact] = useState('lorem ipsum')
+  const [fact, setFact] = useState()
 
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
+    fetch(CAT_ENDPOINT_RAMDOM_FACT  )
       .then(res => res.json())
-      .then(data => setFact(data.fact))
-  }, [])
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstWord = fact.split(' ', 3).join(' ')
+        console.log(firstWord)
+      })
+  }, []);
 
   return (
     <main>
       <h1>App Gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
     </main>
   )
 }
